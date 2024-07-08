ac148777edfd6c0d3923cafbfd84ed4077f97368
bupd <bupdprasanth@gmail.com>
2024-07-08 21:19:07 +0530

    update main go file
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"3sync/internal/auth"
 	"3sync/internal/server"
 	"fmt"
 )
 
 func main() {
+  // Instantiate gothic
+  auth.NewAuth()
 
 	server := server.NewServer()
 
