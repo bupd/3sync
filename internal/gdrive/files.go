8c332cad4d0fa3d114407cd23a264133bb97dbbe
bupd <bupdprasanth@gmail.com>
2024-07-09 00:46:13 +0530

    fix file upload
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- internal/gdrive/files.go
+++ internal/gdrive/files.go
@@ -7,37 +7,48 @@ import (
 	"log"
 	"os"
 
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v2"
 	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
 )
 
-func uploadFile() {
-	client := auth.GetClient()
+// Upload a file to the gdrive
+func UploadFile() {
+	config := &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Endpoint:     google.Endpoint,
+		Scopes:       []string{drive.DriveScope},
+	}
+
+	client := auth.GetClient(config)
 	driveService, err := drive.NewService(context.Background(), option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Unable to create Drive client: %v", err)
 	}
 
-	filePath := "../../hires.jpg"
-	fileExtension := ".jpg"
 	// Open the file to be uploaded
 	currDir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Unable to get current Directory: %v", err)
 	}
 
-	file, err := os.Open(currDir + filePath)
+	filePath := currDir + "/hires.jpg"
+
+	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalf("Unable to open file: %v", err)
 	}
 	defer file.Close()
 
 	fileName := file.Name()
+	log.Println(fileName)
 
 	// File metadata
 	fileMetadata := &drive.File{
-		Title: fileName + fileExtension,
+		Title: "hires.jpg",
 	}
 
 	// Upload the file
